b5c51b8e294246f0691318495660e91fa028f53c
DreamK2826 <jurui2826@qq.com>
2024-05-26 22:32:06 +0800

    20240526暂存版本2,加入了是否批准的dialog,但还未写完

--- app/src/main/java/com/dreamk/newapp1/MyListAdapter.java
+++ app/src/main/java/com/dreamk/newapp1/MyListAdapter.java
@@ -5,9 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+
+import com.gsls.gt.GT;
 
 import java.util.LinkedList;
 
@@ -52,10 +57,36 @@ public class MyListAdapter extends BaseAdapter {
         myViewHolder.tv_l_num.setText(mData.get(position).getlNumber());
         myViewHolder.tv_l_msg.setText(mData.get(position).getlMsg());
         myViewHolder.img.setOnClickListener(v -> {
-
+            permitTest();
             ToastUtil.show(mContext,"imgOnClick:" + position);
         });
         return convertView;
+    }
+
+    /**
+     * 用于弹出管理员页面的是否通过请求 dialog
+     */
+    void permitTest() {
+
+        GT.GT_Dialog.GT_AlertDialog.ViewDialog viewDialog = new GT.GT_Dialog.GT_AlertDialog.ViewDialog()
+                .initLayout(mContext, R.layout.dialog_set_permission, R.style.dialogNoBg, false, 99, 20, 20);
+
+        final LinearLayout dialog_layout = viewDialog.getView().findViewById(R.id.dialog_layout);
+        final TextView tv_dialog_tittle = viewDialog.getView().findViewById(R.id.tv_dialog_tittle);
+        final TextView tv_dialog_uName = viewDialog.getView().findViewById(R.id.tv_dialog_uName);
+        final TextView tv_dialog_uNumber = viewDialog.getView().findViewById(R.id.tv_dialog_uNumber);
+        final TextView tv_dialog_uColor = viewDialog.getView().findViewById(R.id.tv_dialog_uColor);
+        final TextView tv_dialog_uMessage = viewDialog.getView().findViewById(R.id.tv_dialog_uMessage);
+        final Button btn_dialog_reject = viewDialog.getView().findViewById(R.id.btn_dialog_reject);
+        final Button btn_dialog_cancel = viewDialog.getView().findViewById(R.id.btn_dialog_cancel);
+        final Button btn_dialog_OK = viewDialog.getView().findViewById(R.id.btn_dialog_OK);
+
+
+        btn_dialog_cancel.setOnClickListener(v -> viewDialog.getDialog().dismiss());
+
+
+
+        viewDialog.getDialog().show();
     }
 
     static class MyViewHolder{
